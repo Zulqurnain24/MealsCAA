93068ea4aac5deb690bcdd1a6b1e9d9fc52ae9ad
Zulqurn <101562957+Zulqurn@users.noreply.github.com>
2023-05-13 09:59:28 +0100

    Initial Commit

---
+++ MealsCAA/MealsCAAApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealsCAAApp.swift
+//  MealsCAA
+//
+//  Created by Muhammad Zulqurnain on 13/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct MealsCAAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
